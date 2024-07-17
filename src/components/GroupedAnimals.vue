5ecc001d1ce3e2d8397342f4634b2144efc11e16
vivienlehmann <vivien.lehmann@normecgroup.com>
2024-07-17 17:02:36 +0200

    Fix lines

--- src/components/GroupedAnimals.vue
+++ src/components/GroupedAnimals.vue
@@ -2,18 +2,18 @@
   <div class="grouped-animals">
     <div class="grouping-selector">
       <label for="grouping">Group by:</label>
-      <select id="grouping" v-model="selectedGrouping">
-        <option value="type">Type</option>
-        <option value="diet">Diet</option>
-        <option value="dangerLevel">Danger Level</option>
-		  <option value="speed">Speed</option>
-      </select>
+		<select id="grouping" v-model="selectedGrouping">
+			<option value="type">Type</option>
+			<option value="diet">Diet</option>
+			<option value="dangerLevel">Danger Level</option>
+			<option value="speed">Speed</option>
+		</select>
     </div>
     <div v-for="animalGroup in groupedAnimals" :key="animalGroup.group" class="animal-group">
       <h3>{{ animalGroup.group }}</h3>
       <ul>
         <li v-for="animal in animalGroup.animals" :key="animal.id">
-          <strong>{{ animal.name }}</strong> - {{ animal.diet }}, Danger Level: {{ animal.dangerLevel }}, Speed: {{ animal.speed }}
+          <strong>{{ animal.name }}</strong> - Diet: {{ animal.diet }}, Danger Level: {{ animal.dangerLevel }}, Speed: {{ animal.speed }}
         </li>
       </ul>
     </div>
