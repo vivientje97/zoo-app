e4f97cfc03b1c73ffe44985b01a0157805e561a5
vivienlehmann <vivien.lehmann@normecgroup.com>
2024-09-18 12:51:20 +0200

    Split functions

--- src/models/Animal.js
+++ src/models/Animal.js
@@ -18,26 +18,26 @@ export default class Animal {
 	}
 	
 	interactWith(otherAnimal) {
-		if (this.diet === 'Carnivore' && otherAnimal.diet !== 'Carnivore') {
-			return this.handleCarnivoreInteraction(otherAnimal)
-		} else if (this.diet === 'Herbivore') {
-			return this.handleHerbivoreInteraction(otherAnimal)
-		} else if (this.diet === 'Omnivore') {
-			return this.handleOmnivoreInteraction(otherAnimal)
-		} else {
-			return `${this.name} and ${otherAnimal.name.toLowerCase()} interact cautiously.`
+		switch (this.diet) {
+			case 'Carnivore':
+				return this.handleCarnivoreInteraction(otherAnimal)
+			case 'Herbivore':
+				return this.handleHerbivoreInteraction(otherAnimal)
+			case 'Omnivore':
+				return this.handleOmnivoreInteraction(otherAnimal)
+			default:
+				return this.handleNeutralInteraction(otherAnimal)
 		}
 	}
 	
 	handleCarnivoreInteraction(otherAnimal) {
-		if (this.size > otherAnimal.size) {
-			const escapeChance = otherAnimal.speed / (this.speed + otherAnimal.speed)
-			if (Math.random() < escapeChance) {
+		if (this.size >= otherAnimal.size) {
+			if (this.tryToEscape(otherAnimal)) {
 				return `${otherAnimal.name} escapes from ${this.name.toLowerCase()} due to its higher speed!`
 			}
-			const chanceOfEating = this.size / (this.size + otherAnimal.size)
-			if (Math.random() < chanceOfEating) {
-				otherAnimal.eaten = true
+			
+			if (this.tryToEat(otherAnimal)) {
+				otherAnimal.eaten = true;
 				return `${this.name} attacks and eats ${otherAnimal.name.toLowerCase()}!`
 			} else {
 				return `${this.name} tries to catch ${otherAnimal.name.toLowerCase()} but fails.`
@@ -49,53 +49,81 @@ export default class Animal {
 	
 	handleHerbivoreInteraction(otherAnimal) {
 		if (otherAnimal.diet === 'Carnivore') {
-			if (otherAnimal.size > this.size) {
-				return `${this.name} tries to escape from ${otherAnimal.name.toLowerCase()}, sensing danger due to its size.`
-			} else {
-				return `${this.name} cautiously watches ${otherAnimal.name.toLowerCase()}, but feels safe.`
-			}
+			return this.handleHerbivoreVsCarnivore(otherAnimal)
 		} else {
-			if (this.type === 'Fish' || this.type === 'Reptile') {
-				return `${this.name} and ${otherAnimal.name.toLowerCase()} coexist peacefully in their environment.`
-			}
-			return `${this.name} and ${otherAnimal.name.toLowerCase()} graze together peacefully.`
+			return this.handleHerbivoreVsOther(otherAnimal)
 		}
 	}
 	
 	handleOmnivoreInteraction(otherAnimal) {
 		if (otherAnimal.diet === 'Carnivore') {
-			if (otherAnimal.size > this.size) {
-				return `${this.name} cautiously avoids ${otherAnimal.name.toLowerCase()}, sensing danger due to its size.`
-			} else {
-				return `${this.name} cautiously interacts with ${otherAnimal.name.toLowerCase()}, aware of the potential threat.`
-			}
+			return this.handleOmnivoreVsCarnivore(otherAnimal)
 		} else if (otherAnimal.diet === 'Herbivore') {
-			if (this.size > otherAnimal.size) {
-				const escapeChance = otherAnimal.speed / (this.speed + otherAnimal.speed)
-				if (Math.random() < escapeChance) {
-					return `${otherAnimal.name} escapes from ${this.name.toLowerCase()} thanks to its higher speed!`
-				}
-				
-				const chanceOfEating = this.size / (this.size + otherAnimal.size)
-				if (Math.random() < chanceOfEating) {
-					otherAnimal.eaten = true
-					return `${this.name} eats the smaller ${otherAnimal.name.toLowerCase()}.`
-				} else {
-					return `${this.name} tries to eat ${otherAnimal.name.toLowerCase()}, but fails.`
-				}
-			} else {
-				if (this.type === 'Fish' || this.type === 'Reptile') {
-					return `${this.name} and ${otherAnimal.name.toLowerCase()} coexist peacefully in the same space.`
-				}
-				return `${this.name} and ${otherAnimal.name.toLowerCase()} cautiously share resources side by side.`
-			}
+			return this.handleOmnivoreVsHerbivore(otherAnimal)
 		} else {
-			return `${this.name} and ${otherAnimal.name.toLowerCase()} cautiously share resources.`
+			return this.handleNeutralInteraction(otherAnimal)
 		}
 	}
 	
+	handleHerbivoreVsCarnivore(otherAnimal) {
+		if (otherAnimal.size > this.size) {
+			return `${this.name} tries to escape from ${otherAnimal.name.toLowerCase()}, sensing danger due to its size.`
+		} else {
+			return `${this.name} cautiously watches ${otherAnimal.name.toLowerCase()}, but feels safe.`
+		}
+	}
+	
+	handleHerbivoreVsOther(otherAnimal) {
+		if (this.type === 'Fish' || this.type === 'Reptile') {
+			return `${this.name} and ${otherAnimal.name.toLowerCase()} coexist peacefully in their environment.`
+		}
+		return `${this.name} and ${otherAnimal.name.toLowerCase()} graze together peacefully.`
+	}
+	
+	handleOmnivoreVsCarnivore(otherAnimal) {
+		if (otherAnimal.size > this.size) {
+			return `${this.name} cautiously avoids ${otherAnimal.name.toLowerCase()}, sensing danger due to its size.`
+		} else {
+			return `${this.name} cautiously interacts with ${otherAnimal.name.toLowerCase()}, aware of the potential threat.`
+		}
+	}
+	
+	handleOmnivoreVsHerbivore(otherAnimal) {
+		if (this.size >= otherAnimal.size) {
+			if (this.tryToEscape(otherAnimal)) {
+				return `${otherAnimal.name} escapes from ${this.name.toLowerCase()} thanks to its speed!`
+			}
+			
+			if (this.tryToEat(otherAnimal)) {
+				otherAnimal.eaten = true;
+				return `${this.name} eats the smaller ${otherAnimal.name.toLowerCase()}.`
+			} else {
+				return `${this.name} tries to eat ${otherAnimal.name.toLowerCase()}, but fails.`
+			}
+		} else {
+			if (this.type === 'Fish' || this.type === 'Reptile') {
+				return `${this.name} and ${otherAnimal.name.toLowerCase()} coexist peacefully in the same space.`
+			}
+			return `${this.name} and ${otherAnimal.name.toLowerCase()} cautiously share resources side by side.`
+		}
+	}
+	
+	handleNeutralInteraction(otherAnimal) {
+		return `${this.name} and ${otherAnimal.name.toLowerCase()} interact cautiously.`
+	}
+	
+	tryToEscape(otherAnimal) {
+		const escapeChance = otherAnimal.speed / (this.speed + otherAnimal.speed)
+		return Math.random() < escapeChance;
+	}
+	
+	tryToEat(otherAnimal) {
+		const chanceOfEating = this.size / (this.size + otherAnimal.size)
+		return Math.random() < chanceOfEating
+	}
+	
 	isAlive() {
-		return !this.eaten
+		return !this.eaten;
 	}
 	
 	describe() {
